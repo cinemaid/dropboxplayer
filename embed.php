f70429f49468e90e692eff1221553d614edea259
cinemaid <77183850+cinemaid@users.noreply.github.com>
2021-01-10 07:49:39 +0700

    Add files via upload

---
+++ embed.php
@@ -0,0 +1,100 @@
+<?php 
+/**
+ * Name: Dropbox Player Script
+ * Version: 1.1, Last updated: June 2, 2018
+ * Website: https://apicodes.com
+ * Contact: Support@apicodes.com
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dropbox Video Player - APICodes</title>
+	<meta name="robots" content="noindex">
+	<link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon" />
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>
+	<script type="text/javascript" src="https://ssl.p.jwpcdn.com/player/v/8.6.2/jwplayer.js"></script>
+	<script type="text/javascript">jwplayer.key="cLGMn8T20tGvW+0eXPhq4NNmLB57TrscPjd1IyJF84o=";</script>
+	<style type="text/css" media="screen">html,body{padding:0;margin:0;height:100%}#apicodes-player{width:100%!important;height:100%!important;overflow:hidden;background-color:#000}</style>
+</head>
+<body>
+
+<?php 
+		error_reporting(0);
+		
+		$data = (isset($_GET['data'])) ? $_GET['data'] : '';
+
+		if ($data != '') {
+			
+			include_once 'config.php';
+
+			$data = json_decode(decode($data));
+
+			$link = (isset($data->link)) ? $data->link : '';
+
+			$sub = (isset($data->sub)) ? $data->sub : '';
+
+			$poster = (isset($data->poster)) ? $data->poster : '';
+
+			$tracks = '';
+			
+			foreach ($sub as $key => $value) {
+			    $tracks .= '{ 
+						        file: "'.$value.'", 
+						        label: "'.$key.'",
+						        kind: "captions"
+							   },';
+			}
+
+			include_once 'curl.php';
+
+			$curl = new cURL();
+
+			$linkDropbox = str_replace('?dl=0', '', $link);
+
+		    $linkDropbox = $linkDropbox . '?dl=1';
+
+		    $sources = '[{"label":"HD","type":"video\/mp4","file":"'.$linkDropbox.'"}]';
+
+			$result = '<div id="apicodes-player"></div>';
+
+			$data = 'var player = jwplayer("apicodes-player");
+						player.setup({
+							sources: '.$sources.',
+							aspectratio: "16:9",
+							startparam: "start",
+							primary: "html5",
+							autostart: false,
+							preload: "auto",
+							image: "'.$poster.'",
+						    captions: {
+						        color: "#f3f368",
+						        fontSize: 16,
+						        backgroundOpacity: 0,
+						        fontfamily: "Helvetica",
+						        edgeStyle: "raised"
+						    },
+						    tracks: ['.$tracks.']
+						});
+			            player.on("setupError", function() {
+			              swal("Server Error!", "Please contact us to fix it asap. Thank you!", "error");
+			            });
+						player.on("error" , function(){
+							swal("Server Error!", "Please contact us to fix it asap. Thank you!", "error");
+						});';
+			
+			$packer = new Packer($data, 'Normal', true, false, true);
+
+			$packed = $packer->pack();
+
+			$result .= '<script type="text/javascript">' . $packed . '</script>';
+
+			echo $result;
+
+		} else echo 'Empty link!';
+
+?>
+
+</body>
+</html>
